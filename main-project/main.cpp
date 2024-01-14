05a8e4055658c24999c9235024061965b848d149
ddashaa10 <moiseenkod712@gmail.com>
2024-01-14 23:44:07 +0300

    Реализовано чтение данных из файла

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -2,10 +2,46 @@
 
 using namespace std;
 
+#include "book_subscription.h"
+#include "file_reader.h"
+#include "constants.h"
+
 int main()
 {
-    cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #0. Library Subscription\n";
-    cout << "Author: Dasha Moiseenko\n";
+    setlocale(LC_ALL, "Russian");
+    cout << "Лабораторная работа №1. GIT\n";
+    cout << "Вариант №0. Библиотечный абонемент\n";
+    cout << "Автор: Dasha Moiseenko\n\n";
+    book_subscription* subscriptions[MAX_FILE_ROWS_COUNT];
+    int size;
+    try
+    {
+        read("data.txt", subscriptions, size);
+        for (int i = 0; i < size; i++)
+        {
+            cout << subscriptions[i]->reader.last_name << '\n';
+            cout << subscriptions[i]->reader.first_name << '\n';
+            cout << subscriptions[i]->reader.middle_name << '\n';
+            cout << subscriptions[i]->finish.day << ' ';
+            cout << subscriptions[i]->finish.month << ' ';
+            cout << subscriptions[i]->finish.year << '\n';
+            cout << subscriptions[i]->start.day << ' ';
+            cout << subscriptions[i]->start.month << ' ';
+            cout << subscriptions[i]->start.year << '\n';
+            cout << subscriptions[i]->author.last_name << '\n';
+            cout << subscriptions[i]->author.first_name << '\n';
+            cout << subscriptions[i]->author.middle_name << '\n';
+            cout << subscriptions[i]->title << '\n';
+            cout << '\n';
+        }
+        for (int i = 0; i < size; i++)
+        {
+            delete subscriptions[i];
+        }
+    }
+    catch (const char* error)
+    {
+        cout << error << '\n';
+    }
     return 0;
 }
