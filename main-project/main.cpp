7dbf551b76b3202ddbc5658dfe9d39ecc20e53b8
ddashaa10 <moiseenkod712@gmail.com>
2024-01-14 23:49:56 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #0. Library Subscription\n";
     cout << "Author: Dasha Moiseenko\n";
+    cout << "Group: 23ÏÈíæ1ç\n";
     return 0;
 }
